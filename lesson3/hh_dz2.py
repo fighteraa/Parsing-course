38887d51d47e01253115a7ee3c65212209a33527
Andrey <fighteraa@gmail.com>
2019-09-09 23:54:00 +0300

    Парсинг HTML

---
+++ lesson3/hh_dz2.py
@@ -0,0 +1,59 @@
+# 2) Доработать приложение таким образом, чтобы можно
+# было искать разработчиков на разные языки
+# программирования (Например Python, Java, C++)
+
+# Я сначала ищу первые 5 страниц по вакансии программист,
+# и потом из этих вакансий смотрю, есть необходимый язык разработки или нет
+
+import requests
+from lxml import html
+
+
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 '
+                         '(KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+
+
+def request_to_hh():
+    name_prog = input('Введите язык программирования для разработчика: ')
+    all_page_req = []
+    profession = 'Программист'
+    session = requests.Session()
+    for p in range(4):
+        request = session.get('https://spb.hh.ru/search/vacancy', params={'text': profession, 'page': 1}, headers=headers)
+        root = html.fromstring(request.text)
+        results_list = root.xpath("//a[contains(@class, 'bloko-link HH-LinkModifier')]/text() "
+                                  "| //div[contains(@class, 'vacancy-serp-item__compensation')]/text()"
+                                  "| //a[contains(@class, 'bloko-link HH-LinkModifier')]/@href")
+        all_page_req.extend(results_list)
+
+    if all_page_req:
+        f_posit = 0
+        count_prof = 0
+        for k in range(len(all_page_req)):
+            if f_posit == 0:
+                f_posit = 1
+            elif f_posit == 1:
+                if name_prog in all_page_req[k]:
+                    count_prof += 1
+                    print(f'Ссылка на вакансию: {all_page_req[k-1]}')
+                    print(f'Наименование Вакансии: {all_page_req[k]}')
+                    if k < len(all_page_req) - 1:
+                        if not all_page_req[k + 1].find('http'):
+                            f_posit = 0
+                            print(f'Зарплата: Не указана')
+                            print('-' * 50)
+                        else:
+                            f_posit = 2
+                            continue
+            elif f_posit == 2:
+                print(f'Зарплата: {all_page_req[k]}')
+                print('-'*50)
+                f_posit = 0
+    else:
+        print("At your request no results were found. Please, check your request.")
+
+    if count_prof == 0:
+        print(f'По вашему запросу "{name_prog}" ничего не найдено')
+
+request_to_hh()
